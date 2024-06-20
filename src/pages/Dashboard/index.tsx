44a3782067558fc97c8038fb6a40515fff9b71cc
mayconbalves <idd_malves@uolinc.com>
2024-06-20 10:01:36 -0300

    feat(dashboard): created a simple get to registration

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from 'react'
 import Collumns from "./components/Columns";
 import * as S from "./styles";
 import { SearchBar } from "./components/Searchbar";
+import axios from "axios"
+
+
 
 const DashboardPage = () => {
+  const [registrations, setRegistrarion] = useState<any>()
+
+  useEffect(() => {
+    const fetchDate = async() => {
+      const { data } = await axios.get('http://localhost:3000/registrations')
+
+      setRegistrarion(data)
+    }
+
+    fetchDate()
+  }, [])
+
+
+
+  console.log(registrations, 'registration')
   return (
     <S.Container>
       <SearchBar />
-      <Collumns registrations={[]} />
+      <Collumns registrations={registrations} />
     </S.Container>
   );
 };
