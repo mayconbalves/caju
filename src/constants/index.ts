1934060c3e0905913f025659fe7d7ef8a5028fb3
mayconbalves <mayconbalves1@gmail.com>
2024-10-19 11:44:55 -0300

    feat(*): changed endpoint

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
-export const apiUrl = import.meta.env.VITE_API_URL
+import.meta.env.MODE === 'development'
+  ? 'http://localhost:3000'
+  : 'https://gist.githubusercontent.com/mayconbalves/a54bc9f426f086ce1f7b011e8eefca2b/raw/8d3f223b2c7de11c544bb4e2f82cce18e8997ceb/db.json'
 
 export const allColumns = [
   { status: 'REVIEW', title: 'Pronto para revisar' },
