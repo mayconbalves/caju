855886ecbaa3f22f589fe722e8e580992c690ab6
mayconbalves <mayconbalves1@gmail.com>
2024-10-24 20:40:09 -0300

    docs(readme): updated file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/caju/',
+  base: '/',
   plugins: [react(), eslintPlugin()],
   server: {
     port: 3001
